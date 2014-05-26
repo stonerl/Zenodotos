40b18b571f2c34474eb923d592fd7a233ddd265e
Toni Förster <toni.foerster@gmail.com>
2014-05-26 15:55:50 +0200

    changed smtp server-address from IP to DNS-name

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -51,7 +51,7 @@ Zenodotos::Application.configure do
 
   # Email
   config.action_mailer.delivery_method = :smtp
-  config.action_mailer.smtp_settings = { :address => "134.2.3.2", :openssl_verify_mode => 'none'}
+  config.action_mailer.smtp_settings = { :address => "smtpserv.uni-tuebingen.de", :openssl_verify_mode => 'none'}
   config.action_mailer.perform_deliveries = true
 
   # Enable threaded mode
