2c3c43a4f9c25ed0a3736aed0c50074cd797556d
Toni Förster <toni.foerster@gmail.com>
2014-05-26 15:57:34 +0200

    changed default overdue notice to prevent a loss after restart

--- config/initializers/settings.rb
+++ config/initializers/settings.rb
@@ -1,2 +1,37 @@
 #encoding: utf-8
-Settings.defaults[:reminder_mail] = {intro: 'Guten Tag', outro: 'Ihre Bibliothek', :subject => 'Bücher sind überfällig'}
+Settings.defaults[:reminder_mail] = {
+    
+    intro:
+    
+    'Sehr geehrte/r Bibliotheksbenutzer/in,
+    
+    die nachfolgend genannten Bücher sind zur Rückgabe fällig:',
+    
+    outro:
+    
+    'Bitte bringen Sie diese zurück, oder verlängern Sie die fälligen Medien baldmöglichst.
+    Es sind zwei Verlängerungen möglich. Wenn Sie auf unsere Mahnungen nicht reagieren
+    sollten, behalten wir uns vor, Sie von der Ausleihe in der Fachbibliothek auszuschließen.
+    Sie können während der Zeit, in der eine Mahnfrist bei Ihnen läuft keine weiteren Bücher
+    aus unserer Bibliothek ausleihen. In besonders gravierenden Fällen setzen wir uns mit der
+    Universitäts-Bibliothek in Verbindung, so dass Ihr Konto auch dort gesperrt werden kann.
+    
+    Die aktuellen Öffnungs- und Ausleihzeiten der Bibliothek finden Sie auf der Homepage der
+    Japanologie unter: http://www.uni-tuebingen.de/de/6147
+    
+    Mit freundlichen Grüßen
+    Ihre Fachbibliothek Japanologie
+    
+    bibliothek@japanologie.uni-tuebingen.de
+    
+    EBERHARD KARLS UNIVERSITÄT TÜBINGEN
+    AOI Asien-Orient-Institut
+    Abteilung für Japanologie
+    Wilhelmstr. 90
+    72074 Tübingen
+    Germany',
+    
+    :subject => 'Bücher sind überfällig'
+    
+}
+
